690a8a4c96872fd707f6f5c07a68135e471f1dbc
Majey <kevinerick78@yahoo.com>
2022-01-13 19:29:09 +0300

    DELETE  method

--- routes/product.js
+++ routes/product.js
@@ -31,15 +31,15 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
     }
 });
 
-// //DELETE
-// router.delete("/:id", verifyTokenAndAuthorization, async (req,res) => {
-//     try{
-//         await User.findByIdAndDelete(req.params.id);
-//         res.status(200).json("User has been deleted ...");
-//     }catch(err){
-//         res.status(500).json(err)
-//     }
-// } )
+//DELETE
+router.delete("/:id", verifyTokenAndAdmin, async (req,res) => {
+    try{
+        await Product.findByIdAndDelete(req.params.id);
+        res.status(200).json("Product has been deleted ...");
+    }catch(err){
+        res.status(500).json(err)
+    }
+} )
 
 // //GET USER
 // router.get("/find/:id", verifyTokenAndAdmin, async (req,res) => {
